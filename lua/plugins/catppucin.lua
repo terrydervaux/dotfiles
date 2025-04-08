ca851926786ade7a5877d50b5cd57c9de117b2e0
Terry Dervaux <terry.dervaux@gmail.com>
2025-04-08 14:03:19 +0200

    feat: switch catppucin theme

--- lua/plugins/catppucin.lua
+++ lua/plugins/catppucin.lua
@@ -1,9 +1,9 @@
-return { 
+return {
   "catppuccin/nvim",
   lazy = false,
   name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin"
+    vim.cmd.colorscheme "catppuccin-mocha"
   end
 }
