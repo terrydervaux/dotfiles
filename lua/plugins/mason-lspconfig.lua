0afe2ef668830c6667847c001138e76c1c830156
Terry Dervaux <terry.dervaux@gmail.com>
2025-04-08 12:00:34 +0200

    feat: initial config

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer" },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      'saghen/blink.cmp'
+    },
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local lspconfig = require("lspconfig")
+
+      -- lsp install
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+
+      -- keybinding
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+      -- tg hack
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local c = vim.lsp.get_client_by_id(args.data.client_id)
+          if not c then return end
+
+          if vim.bo.filetype == "lua" then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+              end,
+            })
+          end
+        end,
+      })
+
+      -- display error inline
+      vim.diagnostic.config({
+         virtual_text = {
+           prefix = '●', -- Could be '●', '▎', 'x'
+           spacing = 2,
+         },
+         signs = true,
+         underline = true,
+         update_in_insert = false,
+         severity_sort = true,
+      })
+    end
+  }
+}
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,15 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- system keyboard into P
+vim.opt.clipboard = "unnamedplus"
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.o.guifont = "Source Code Pro:h12:#h-slight"
