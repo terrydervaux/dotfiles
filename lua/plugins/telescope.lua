b1317b00a0b4d5db3b6c7a0d3b4f73f0d6ab7682
Terry Dervaux <terry.dervaux@gmail.com>
2025-04-14 11:25:31 +0200

    feat: add telescope shortcuts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,9 +12,15 @@ return {
     config = function()
       builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      --vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      --vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags'})
+      vim.keymap.set('n', '<leader>en', function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end
   },
   {
