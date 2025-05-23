35d3927eac67433bdd3d9eb9790b11209f04771f
Terry Dervaux <terry.dervaux@gmail.com>
2025-05-23 11:40:40 +0200

    feat: respect .editorconfig files
    Signed-off-by: Terry Dervaux <terry.dervaux@gmail.com>

--- lua/plugins/style.lua
+++ lua/plugins/style.lua
@@ -1,16 +1,19 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.markdownlint,
-        null_ls.builtins.formatting.prettier,
-      },
-    })
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.markdownlint,
+          null_ls.builtins.formatting.prettier,
+        },
+      })
 
-    -- keymap
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+      -- keymap
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
+  { "editorconfig/editorconfig-vim" },
 }
