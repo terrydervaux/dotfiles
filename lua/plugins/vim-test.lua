908a8f3322796a8c36c74a98be0f62ef4da2872d
Terry Dervaux <terry.dervaux@gmail.com>
2025-07-05 12:05:52 +0200

    feat: add vim test

---
+++ lua/plugins/vim-test.lua
@@ -0,0 +1,11 @@
+return {
+  "vim-test/vim-test",
+  dependencies = {
+    "preservim/vimux",
+  },
+  vim.keymap.set("n", "<leader>t", ":TestNearest<CR>"),
+  vim.keymap.set("n", "<leader>T", ":TestFile<CR>"),
+  vim.keymap.set("n", "<leader>a", ":TestSuite<CR>"),
+  vim.keymap.set("n", "<leader>l", ":TestLast,<CR>"),
+  vim.cmd("let test#strategy = 'vimux'"),
+}
