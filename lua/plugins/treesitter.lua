529fe8a2f2ed0caa1ce2c2d8497ef49646550531
Terry Dervaux <terry.dervaux@gmail.com>
2025-09-12 22:26:25 +0200

    feat: add better syntax highlight for yaml

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "rust", "lua", "vim", "vimdoc", "bash", "markdown", "markdown_inline", "bp" },
+      ensure_installed = { "rust", "lua", "vim", "vimdoc", "bash", "markdown", "markdown_inline", "bp", "yaml" },
       sync_install = false,
       indent = { enable = true },
       highlight = {
