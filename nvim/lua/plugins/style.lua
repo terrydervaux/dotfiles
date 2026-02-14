73332f82148e47a9444ecae9ad58dc4af05edfb2
Terry Dervaux <terry.dervaux@gmail.com>
2026-02-14 14:56:59 +0100

    feat: use markdownlint only for diagnostics
    Signed-off-by: Terry Dervaux <terry.dervaux@gmail.com>

--- nvim/lua/plugins/style.lua
+++ nvim/lua/plugins/style.lua
@@ -6,7 +6,7 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.markdownlint,
+          null_ls.builtins.diagnostics.markdownlint,
           null_ls.builtins.formatting.prettier,
         },
       })
