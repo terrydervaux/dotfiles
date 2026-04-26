8e043de53de6fadda76d21bd79e763f0ba4dfafd
Terry Dervaux <terry.dervaux@gmail.com>
2026-04-26 13:52:21 +0200

    refactor: migration treesitter for nvim 0.12.0+
    Signed-off-by: Terry Dervaux <terry.dervaux@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,40 +3,15 @@ return {
   branch = "main",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "rust",
-        "lua",
-        "vim",
-        "vimdoc",
-        "bash",
-        "markdown",
-        "markdown_inline",
-        "bp",
-        "yaml",
-        "python",
-        "toml",
-        "ron",
-      },
-      sync_install = false,
-      indent = { enable = true },
-      highlight = {
-        enable = true,
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(ev)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
+        if ok and stats and stats.size > max_filesize then
+          return
+        end
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
